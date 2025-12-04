8d68093d8413faa5a3eabb9db9366765c4ff516e
paka <wjd1357912@gmail.com>
2025-12-04 20:34:18 +0900

    정렬

---
+++ src/main/java/com/alpaca/CoTe/B11652_카드.java
@@ -0,0 +1,43 @@
+package com.alpaca.CoTe;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class B11652_카드 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long[] tmp = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            tmp[i] = Long.parseLong(br.readLine());
+        }
+        Arrays.sort(tmp);
+
+        int cnt = 0;
+        long mxVal = (long) (-Math.pow(2, 62)- 1);
+        int mxCnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || tmp[i - 1] == tmp[i]) {
+                cnt++;
+            } else {
+                if (cnt > mxCnt) {
+                    mxCnt = cnt;
+                    mxVal = tmp[i - 1];
+                }
+                cnt = 1;
+            }
+        }
+        if (cnt > mxCnt) {
+            mxVal = tmp[n - 1];
+        }
+        System.out.println(mxVal);
+    }
+}
---
+++ src/main/java/com/alpaca/CoTe/B15688_수_정렬하기.java
@@ -0,0 +1,53 @@
+package com.alpaca.CoTe;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B15688_수_정렬하기 {
+    /*
+    countingSort*/
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int[] count = new int[2000001];
+
+        for(int i=0; i<n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            count[num+1000000]++;
+        }
+
+        for(int i=0; i<count.length; i++) {
+            while (count[i] > 0) {
+                sb.append(i - 1000000).append('\n');
+                count[i]--;
+            }
+        }
+
+        System.out.print(sb);
+
+    }
+
+    /* 내 풀이
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] a= new int[n];
+        for(int i =0; i<n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        int i=0;
+        Arrays.sort(a);
+        while(i <n){
+        sb.append(a[i++]).append(" ");}
+        System.out.println(sb);
+    }
+     */
+
+}
