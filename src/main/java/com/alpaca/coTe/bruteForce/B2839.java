47da39d22f86e19ec62acfcf19ed8ca441d19ab9
GARDEN\wjd13 <wjd1357912@gmail.com>
2025-12-24 10:23:23 +0900

    no message

---
+++ src/main/java/com/alpaca/coTe/bruteForce/B2839.java
@@ -0,0 +1,29 @@
+package com.alpaca.coTe.bruteForce;
+
+import java.util.Scanner;
+
+public class B2839 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        while(true){
+            if(n%5 ==0){
+                count += n/5;
+                System.out.println(count);
+                break;
+            }
+
+            n -= 3;
+            count++;
+            if(n<0) {
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}
